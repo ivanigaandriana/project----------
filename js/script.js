80fb2f3abe18aced62496cb769d0baf348fff1dc
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2023-11-28 18:25:22 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const swiper = new Swiper('.material__slaider', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+});
+
